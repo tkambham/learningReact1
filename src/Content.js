0b932b0f5f9192b289e9a34b0594d995dfa2745b
trilok <trilokkambham2001@gmail.com>
2025-02-02 12:21:41 -0600

    React useState

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 const Content = () => {
+    const [name, setname] = useState("Trilok");
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () =>{
         const names = ["Trilok", "Rahul", "Rohit", "Rajesh"];
         const int = Math.floor(Math.random() * 4);
-        return names[int];
+        setname(names[int]);
     }
+
+    const handleClick = () =>{
+        console.log(count);
+        setCount(count + 1);
+    }
+    const handleClick2 = (name) =>{
+        console.log(`${name} was clicked`);
+    }
+
     return (
         <main>
             <p>
-                Hello {handleNameChange()}!, Welcome to the world of React.
+                Hello {name}!, Welcome to the world of React.
             </p>
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>Click Me</button>
         </main>
     )
 }
