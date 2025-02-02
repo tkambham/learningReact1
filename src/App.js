a3b7f5c668ffaa87e51ec2237e1e05bd39aa892a
trilok <trilokkambham2001@gmail.com>
2025-02-01 20:58:02 -0600

    using javacipt expressions and functions

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Trilok";
+  const handleNameChange = () =>{
+    const names = ["Trilok", "Rahul", "Rohit", "Rajesh"];
+    const int = Math.floor(Math.random() * 4);
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameChange()}!</p>
       </header>
     </div>
   );
